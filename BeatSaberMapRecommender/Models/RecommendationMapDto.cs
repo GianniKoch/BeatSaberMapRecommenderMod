a6207b7ae83d6a3f6ef246b9fb9f08786e0730fb
GianniKoch <johannes.koch@student.kdg.be>
2023-04-12 11:46:30 +0200

    Updated the model to optimize list loading with pages and buttons.

--- BeatSaberMapRecommender/Models/RecommendationMapDto.cs
+++ BeatSaberMapRecommender/Models/RecommendationMapDto.cs
@@ -9,17 +9,23 @@ namespace BeatSaberMapRecommender.Models
 			[JsonProperty("characteristic")] int? characteristic,
 			[JsonProperty("difficulty")] int? difficulty,
 			[JsonProperty("meta_sim")] double? metaSim,
-			[JsonProperty("song_id")] string songId,
+			[JsonProperty("song_id")] string songKey,
 			[JsonProperty("tag_sim")] double? tagSim,
-			[JsonProperty("total_sim")] double? totalSim
+			[JsonProperty("total_sim")] double? totalSim,
+			[JsonProperty("map_name")] string mapName,
+			[JsonProperty("uploader_name")] string uploader,
+			[JsonProperty("cover_url")] string coverUrl
 		)
 		{
 			Characteristic = characteristic;
 			Difficulty = difficulty;
 			MetaSim = metaSim;
-			SongId = songId;
+			SongKey = songKey;
 			TagSim = tagSim;
 			TotalSim = totalSim;
+			MapName = mapName;
+			Uploader = uploader;
+			CoverUrl = coverUrl;
 		}
 
 		[JsonProperty("characteristic")] public int? Characteristic { get; }
@@ -28,10 +34,16 @@ namespace BeatSaberMapRecommender.Models
 
 		[JsonProperty("meta_sim")] public double? MetaSim { get; }
 
-		[JsonProperty("song_id")] public string SongId { get; }
+		[JsonProperty("song_id")] public string SongKey { get; }
 
 		[JsonProperty("tag_sim")] public double? TagSim { get; }
 
 		[JsonProperty("total_sim")] public double? TotalSim { get; }
+
+		[JsonProperty("map_name")] public string MapName { get; }
+
+		[JsonProperty("uploader_name")] public string Uploader { get; }
+
+		[JsonProperty("cover_url")] public string CoverUrl { get; }
 	}
 }
